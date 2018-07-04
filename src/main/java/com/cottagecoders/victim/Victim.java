983b1eb255f0fc9eee2b8357c342301da511ff78
bob plotts <robertcplotts@gmail.com>
2018-07-03 20:35:39 -0400

    first commit - test program

---
+++ src/main/java/com/cottagecoders/victim/Victim.java
@@ -0,0 +1,52 @@
+package com.cottagecoders.victim;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Victim {
+
+  private static Logger log = LoggerFactory.getLogger(Victim.class);
+
+  private Victim() {
+    log.info("Victim ctor got here.");
+
+  }
+
+  public static void main(String[] args) {
+    method1();
+    Victim victim = new Victim();
+
+    for (int i = 0; i < 5; i++) {
+      victim.run1();
+      victim.run2();
+    }
+
+  }
+
+  private static void method1() {
+    log.info("got here.");
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException ex) {
+      log.error(ex.getMessage());
+    }
+  }
+
+  private void run1() {
+    log.info("Victim run1 got here.");
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException ex) {
+      log.error(ex.getMessage());
+    }
+  }
+
+  private void run2() {
+    log.info("Victim run2 got here.");
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException ex) {
+      log.error(ex.getMessage());
+    }
+  }
+}
